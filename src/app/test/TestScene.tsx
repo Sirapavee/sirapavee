281743837666947e9e57e1656481e88564ee6cb4
Sirapavee <sirapavee@gmail.com>
2026-02-24 23:53:09 +0700

    feat(3js): hyperspace 3d experiment

--- src/app/test/TestScene.tsx
+++ src/app/test/TestScene.tsx
@@ -1,58 +1,278 @@
 'use client';
 
-import { useMemo, useRef } from 'react';
-import {
-  ContactShadows,
-  Float,
-  OrbitControls,
-  Plane,
-  useHelper,
-} from '@react-three/drei';
+import { FC, useEffect, useMemo, useRef, useState } from 'react';
+import { Html, Instance, Instances, OrbitControls } from '@react-three/drei';
 import { Canvas, useFrame } from '@react-three/fiber';
-import { useControls } from 'leva';
-import { Mesh, PointLight, PointLightHelper, Scene } from 'three';
+import { useReactiveGetCookie } from 'cookies-next';
+import {
+  MathUtils,
+  Mesh,
+  PerspectiveCamera,
+  PointsMaterial,
+  Scene,
+  SphereGeometry,
+} from 'three';
 
-const Box = () => {
-  const meshRef = useRef<Mesh>(null);
-  const lightRef = useRef<PointLight>(null!);
+import { themeBgReverse, themeSubHeader } from '@/const/tailwindClass';
+import { cn } from '@/utils/className';
 
-  const { color, width, height, depth } = useControls('Box', {
-    color: '#00ffff',
-    width: { value: 1, min: 0.5, max: 3, step: 0.1 },
-    height: { value: 1, min: 0.5, max: 3, step: 0.1 },
-    depth: { value: 1, min: 0.5, max: 3, step: 0.1 },
-  });
+// const Box = () => {
+//   const meshRef = useRef<Mesh>(null);
+//   const lightRef = useRef<PointLight>(null!);
 
-  // const boxLengthRef = useRef<number>(1);
+//   const { color, width, height, depth } = useControls('Box', {
+//     color: '#00ffff',
+//     width: { value: 1, min: 0.5, max: 3, step: 0.1 },
+//     height: { value: 1, min: 0.5, max: 3, step: 0.1 },
+//     depth: { value: 1, min: 0.5, max: 3, step: 0.1 },
+//   });
+
+//   // const boxLengthRef = useRef<number>(1);
+
+//   useFrame(() => {
+//     if (meshRef.current) {
+//       meshRef.current.rotation.x += 0.01;
+//       meshRef.current.rotation.y += 0.01;
+
+//       meshRef.current.position.x = Math.sin(meshRef.current.rotation.y) * 2;
+//       // meshRef.current.scale.x += 0.01;
+
+//       // if (boxLengthRef.current <= 3) {
+//       //   boxLengthRef.current += 0.01;
+//       //   meshRef.current.scale.x += 0.01;
+//       // } else if (boxLengthRef.current > 10) {
+//       //   boxLengthRef.current -= 0.01;
+//       //   meshRef.current.scale.x -= 0.01;
+//       // }
+//     }
+//   });
+
+//   useHelper(lightRef, PointLightHelper, 0.5, 'hotpink');
+
+//   return (
+//     <>
+//       <mesh ref={meshRef} position={[0, 1, 0]}>
+//         <boxGeometry args={[width, height, depth]} />
+//         <meshStandardMaterial color={color} />
+//       </mesh>
+
+//       <ambientLight intensity={0.5} />
+//       <pointLight ref={lightRef} intensity={100} color='white' position={[2, 1, 3]} />
+//     </>
+//   );
+// };
+
+type ConfigProps = {
+  configValue: {
+    [key: string]: number;
+  };
+  mode: 'start' | 'cleaning' | 'stop';
+};
+
+type StarProps = {
+  configProps: ConfigProps;
+};
+const Star: FC<StarProps> = ({ configProps }) => {
+  const starRef = useRef<Mesh>(null);
+
+  const defaultPositionZ = MathUtils.randFloatSpread(100);
+
+  const resetStarPosition = () => {
+    if (starRef.current && starRef.current.position.z > 50) {
+      starRef.current.position.z = -50;
+    }
+  };
 
   useFrame(() => {
-    if (meshRef.current) {
-      meshRef.current.rotation.x += 0.01;
-      meshRef.current.rotation.y += 0.01;
-      // meshRef.current.scale.x += 0.01;
+    if (starRef.current) {
+      starRef.current.position.z += 0.1;
 
-      // if (boxLengthRef.current <= 3) {
-      //   boxLengthRef.current += 0.01;
-      //   meshRef.current.scale.x += 0.01;
-      // } else if (boxLengthRef.current > 10) {
-      //   boxLengthRef.current -= 0.01;
-      //   meshRef.current.scale.x -= 0.01;
+      // starRef.current.scale.z += 0.1;
+      // starRef.current.position.z += 1;
+
+      if (starRef.current.position.z > 25) {
+        starRef.current.scale.set(0, 0, 0);
+      }
+
+      if (starRef.current.position.z > 45) {
+        starRef.current.scale.x += 0.5;
+        starRef.current.scale.y += 0.5;
+        starRef.current.scale.z += 0.5;
+      }
+
+      // if (starRef.current.position.z > 50) {
+      //   starRef.current.position.z = -50;
       // }
+
+      if (starRef.current.scale.x === 1) {
+        starRef.current.scale.set(1, 1, 1);
+      }
+
+      if (configProps.mode === 'start') {
+        Object.entries(configProps.configValue).forEach(([key, value]) => {
+          if (starRef.current) {
+            if (key === 'position.z') {
+              // starRef.current.position.z -= 0.4;
+
+              setTimeout(() => {
+                starRef.current!.position.z += value;
+              }, 1000);
+            } else if (key === 'scale.z') {
+              starRef.current.scale.z += value;
+            }
+          }
+        });
+
+        resetStarPosition();
+      } else if (configProps.mode === 'cleaning') {
+        starRef.current.position.z = defaultPositionZ;
+        starRef.current.scale.set(0, 0, 0);
+
+        resetStarPosition();
+
+        // starRef.current.visible = false;
+      } else if (configProps.mode === 'stop') {
+        // starRef.current.position.z += 0.1;
+        // starRef.current.scale.set(1, 1, 1);
+
+        // starRef.current.visible = true;
+        resetStarPosition();
+      }
     }
   });
 
-  useHelper(lightRef, PointLightHelper, 0.5, 'hotpink');
+  return (
+    <group>
+      <Instance
+        ref={starRef}
+        position={[
+          MathUtils.randFloatSpread(100),
+          MathUtils.randFloatSpread(100),
+          defaultPositionZ,
+        ]}
+      />
+    </group>
+  );
+};
+
+const Galaxy = () => {
+  const [themedColor, setThemeColor] = useState<string>('white');
+
+  const [configProps, setConfigProps] = useState<ConfigProps>({
+    configValue: {
+      ['position.z']: 0,
+      ['scale.z']: 0,
+    },
+    mode: 'stop',
+  });
+
+  const getCookie = useReactiveGetCookie();
+  const theme = getCookie('theme');
+
+  useEffect(() => {
+    setThemeColor(theme === 'dark' ? 'white' : 'black');
+  }, [theme]);
 
   return (
     <>
-      <mesh ref={meshRef} position={[0, 1, 0]}>
-        <boxGeometry args={[width, height, depth]} />
-        <meshStandardMaterial color={color} />
-      </mesh>
+      <Html>
+        <button
+          className={cn(
+            themeBgReverse,
+            themeSubHeader,
+            'absolute rounded-md px-3 py-1 text-sm font-semibold hover:cursor-pointer',
+          )}
+          onMouseOver={() => {
+            setConfigProps({
+              configValue: {
+                ['position.z']: 1,
+                ['scale.z']: 0.5,
+              },
+              mode: 'start',
+            });
+          }}
+          onMouseLeave={() => {
+            setConfigProps({
+              configValue: {
+                ['position.z']: 0,
+                ['scale.z']: 0,
+              },
+              mode: 'cleaning',
+            });
 
-      <ambientLight intensity={0.5} />
-      <pointLight ref={lightRef} intensity={100} color='white' position={[2, 1, 3]} />
+            setTimeout(() => {
+              setConfigProps({
+                configValue: {
+                  ['position.z']: 0,
+                  ['scale.z']: 0,
+                },
+                mode: 'stop',
+              });
+            }, 500);
+          }}
+          onTouchStart={() => {
+            setConfigProps({
+              configValue: {
+                ['position.z']: 1,
+                ['scale.z']: 0.5,
+              },
+              mode: 'start',
+            });
+          }}
+          onTouchEnd={() => {
+            setConfigProps({
+              configValue: {
+                ['position.z']: 0,
+                ['scale.z']: 0,
+              },
+              mode: 'cleaning',
+            });
+
+            setTimeout(() => {
+              setConfigProps({
+                configValue: {
+                  ['position.z']: 0,
+                  ['scale.z']: 0,
+                },
+                mode: 'stop',
+              });
+            }, 500);
+          }}
+        >
+          Hola
+        </button>
+      </Html>
+      <Instances
+        limit={1000}
+        // range={1000}
+        geometry={new SphereGeometry(0.2, 16, 16)}
+        material={new PointsMaterial({ color: themedColor })}
+      >
+        {Array.from(
+          {
+            length: 1000,
+          },
+          (_, i) => (
+            <Star key={i} configProps={configProps} />
+          ),
+        )}
+      </Instances>
     </>
+  );
+};
+
+const Camera = () => {
+  const perspectiveCameraRef = useRef<PerspectiveCamera>(null!);
+  // useHelper(perspectiveCameraRef, CameraHelper);
+
+  return (
+    <perspectiveCamera
+      ref={perspectiveCameraRef}
+      position={[-6, 7, 7]}
+      fov={100}
+      near={0.1}
+      far={500}
+    />
   );
 };
 
@@ -67,22 +287,13 @@ export const TestScene = () => {
         fallback={<div>Sorry no WebGL supported!</div>}
         scene={scene}
       >
-        <Float speed={1} rotationIntensity={1} floatIntensity={2}>
+        {/* <Float speed={1} rotationIntensity={1} floatIntensity={2}>
           <Box />
-        </Float>
+        </Float> */}
 
-        <ContactShadows position={[0, -1, 0]} opacity={1} scale={10} blur={1} far={10} />
+        <Galaxy />
 
-        <Plane position={[0, -2, 0]} rotation={[-Math.PI / 2, 0, 0]} args={[100, 100]} />
-        {/* <instancedMesh ref={instancedMeshRef} args={[undefined, undefined, 100]}>
-          <sphereGeometry args={[1, 32, 32]} />
-          <pointsMaterial color='hotpink' size={0.1} />
-        </instancedMesh>
-        <points position={[2, 0, 0]} name='point-star'>
-          <sphereGeometry args={[1, 1, 1]} />
-          <pointsMaterial color='hotpink' size={0.1} />
-        </points> */}
-        <perspectiveCamera position={[-6, 7, 7]} fov={100} near={0.1} far={1000} />
+        <Camera />
         <OrbitControls />
       </Canvas>
     </div>
