7ea93bb5adfc54d3f42709f4f9e7acfe7f9a2a42
Sirapavee <sirapavee@gmail.com>
2026-02-24 23:52:10 +0700

    feat: setup supabase

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  transpilePackages: ["three"],
+  transpilePackages: ['three'],
+  env: {
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+  },
 };
 
 export default nextConfig;
---
+++ src/app/experience/page.tsx
@@ -0,0 +1,18 @@
+// import { cookies } from 'next/headers';
+
+// import { createClient } from '@/utils/supabase/server';
+
+export default async function ExperiencePage() {
+  //   const cookieStore = cookies();
+  //   const supabase = createClient(cookieStore);
+
+  //   const { data: experienceList } = await supabase
+  //     .from('[sirapavee] experience')
+  //     .select('*');
+
+  //   console.log({
+  //     experienceList,
+  //   });
+
+  return <div>Experience Page</div>;
+}
